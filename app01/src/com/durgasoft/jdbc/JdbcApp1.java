80d1f48328824f28efde7527030829d1ebef10a1
Asus <Asus@LAPTOP-2IPP0FD3>
2020-07-20 00:59:13 +0530

    First commit

---
+++ app01/src/com/durgasoft/jdbc/JdbcApp1.java
@@ -0,0 +1,38 @@
+ package com.durgasoft.jdbc;
+ import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class JdbcApp1 {
+
+	public static void main(String[] args)throws Exception {
+	  //Load and Register the driver
+		Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+		
+      //Establish the connection between java application and database  
+        Connection con=DriverManager.getConnection("jdbc:odbc:aman","system","1234");
+     
+      //Create statement object
+        Statement st=con.createStatement();
+        
+      //Create Buffered reader and take table name as a dynamic input
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Table Name   :");
+        String table_Name=br.readLine();
+        
+      //Write SQL query
+        String query="create table "+table_Name+"(ENO number(3) primary key,ENAME varchar2(10),ESAL float,EADDR varchar2(10))";
+       
+      //Execute query
+        st.executeUpdate(query);
+        
+        System.out.println("Table create successfully .");
+      //close connection
+        st.close();
+        con.close();
+        
+	}
+
+}
